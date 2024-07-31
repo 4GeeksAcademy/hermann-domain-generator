7fe16daab6f344f16c02fb6f5a2233ce2accf364
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-07-31 19:16:31 +0000

    trabajo finalizado

--- src/app.js
+++ src/app.js
@@ -9,3 +9,33 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+function generarDominio(){
+  let dominio = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+          dominio.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+  return dominio;
+}
+
+function desplegarDominios(){
+  let dominios = generarDominio();
+  let listaDominios = document.getElementById('listaDominios');
+
+  dominios.forEach(dominio => {
+    let listElemento = document.createElement("li");
+    listElemento.textContent = dominio;
+    listaDominios.appendChild(listElemento);
+  });
+}
+
+window.onload = desplegarDominios;
