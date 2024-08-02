47ae275a4eb2840ea1150713174f94b4a5da9bc1
hermannramos <140882889+hermannramos@users.noreply.github.com>
2024-08-02 19:25:46 +0000

    tarjetas finalizadas

--- src/app.js
+++ src/app.js
@@ -6,39 +6,76 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const posts = [
+    {
+      "userId": 1,
+      "id": 1,
+      "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+      "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+    },
+    {
+      "userId": 1,
+      "id": 2,
+      "title": "qui est esse",
+      "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+    },
+    {
+      "userId": 1,
+      "id": 3,
+      "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+      "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+    },
+    {
+      "userId": 1,
+      "id": 4,
+      "title": "eum et est occaecati",
+      "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+    },
+    {
+      "userId": 1,
+      "id": 5,
+      "title": "nesciunt quas odio",
+      "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+    },
+    {
+      "userId": 1,
+      "id": 6,
+      "title": "dolorem eum magni eos aperiam quia",
+      "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+    },
+    {
+      "userId": 1,
+      "id": 7,
+      "title": "magnam facilis autem",
+      "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
+    }
+  ]
+  generarCarta(posts);
 };
 
-let pronoun = ['the', 'our'];
-let adj = ['great', 'big'];
-let noun = ['jogger', 'racoon'];
-let ext = ['.com', '.net', '.us', '.io']
+function generarCarta(posts){
+  let tarjetas = document.getElementById('tarjetas');
+  tarjetas.innerHTML = '';
 
-function generarDominio(){
-  let dominio = [];
+  posts.forEach(post => {
+    const tarjeta = document.createElement('div');
+    tarjeta.classList.add('tarjeta');
 
-  for (let i = 0; i < pronoun.length; i++) {
-    for (let j = 0; j < adj.length; j++) {
-        for (let k = 0; k < noun.length; k++) {
-          for ( let x = 0; x < ext.length; x++){
-            dominio.push(pronoun[i] + adj[j] + noun[k] + ext[x]);
-          }
-      }
-    }
-  }
-  return dominio;
-}
+    const img = document.createElement('img');
+    img.src = `https://randomuser.me/api/portraits/men/${post.id}.jpg`
 
-function desplegarDominios(){
-  let dominios = generarDominio();
-  let listaDominios = document.getElementById('listaDominios');
+    const nombre = document.createElement('h2');
+    nombre.textContent = post.title;
 
-  dominios.forEach(dominio => {
-    let listElemento = document.createElement("li");
-    listElemento.textContent = dominio;
-    listaDominios.appendChild(listElemento);
+    const descrip = document.createElement('p');
+    descrip.textContent = post.body;
+
+    tarjeta.appendChild(nombre);
+    tarjeta.appendChild(img);
+    tarjeta.appendChild(descrip);
+
+    tarjetas.appendChild(tarjeta);
   });
 }
 
-window.onload = desplegarDominios;
+generarCarta;
